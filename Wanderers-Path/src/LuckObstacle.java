4dc376219f340d8c1fe7e833f8cc62447f615642
Anupa <anupa@macbookpro.home>
2025-04-08 20:06:48 -0400

    Updated LuckObstacle scenario to make it more interactive.

--- Wanderers-Path/src/LuckObstacle.java
+++ Wanderers-Path/src/LuckObstacle.java
@@ -16,20 +16,30 @@ public class LuckObstacle extends Scenario {
     @Override
     public Scenario run(Player player, Game game) {
         Narrator.lineSeparator();
-        Narrator.talk("Infront of you lays a cursed tomb. Do you dare to approach?");
-        Narrator.enterContinue();
-    
-        int luckCheck = 15;
-        if (player.getLuck() >= luckCheck) {
-             int luckBonus = 2;
-            Narrator.talk("You approach the cursed tomb and discover a ninja star. Your luck increases by " + luckBonus);
-            NinjaStar ninjaStar = new NinjaStar();
-            ninjaStar.applyBonus(player, luckBonus);
-            player.collectItem(ninjaStar);
+        Narrator.talk("Infront of you lays a tomb. Do you dare to approach?");
+        Narrator.talk("Type 'approach' to approach the tomb, or 'continue' to continue along your journey.");
+        String playerChoice = Narrator.getInput().toLowerCase();
+            
+        if (playerChoice.equals("approach")) {
+            int luckCheck = 15;
+            
+            if (player.getLuck() >= luckCheck) {
+                int luckBonus = 2;
+                Narrator.talk("You approach the tomb and discover a ninja star. Your luck increases by " + luckBonus);
+                NinjaStar ninjaStar = new NinjaStar();
+                ninjaStar.applyBonus(player, luckBonus);
+                player.collectItem(ninjaStar);
+            } else {
+                int damage = 10;
+                Narrator.talk("You approach the tomb and get cursed! You take " + damage + " damage");
+                player.takeDamage(damage);
+            }
+        } else if (playerChoice.equals("continue")) {
+            Narrator.talk("You decide to stay away from the tomb and continue along your journey.");
         } else {
-            int damage = 10;
-            Narrator.talk("You approach the cursed tomb and get cursed! You take " + damage + " damage");
-            player.takeDamage(damage);
+            //invalid input
+            Narrator.talk("Invalid choice. Type 'approach' or 'continue'.");
+            return run(player, game);
         }
         
         Narrator.askToCheck(player);
